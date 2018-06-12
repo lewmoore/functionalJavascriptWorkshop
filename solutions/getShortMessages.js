9b3724cbb4573a21fcb9b0281e4a2f5aa6e3e010
Lewis Moore <lewis.moore2707@gmail.com>
2018-06-12 10:49:23 +0100

    Add fourth solution

---
+++ solutions/getShortMessages.js
@@ -0,0 +1,6 @@
+function getShortMessages(messages) {
+    return messages.filter(message => message.message.length < 50 )
+                   .map(message => message.message)
+  }
+
+  module.exports = getShortMessages
